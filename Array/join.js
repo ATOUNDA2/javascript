5d39d09ed43b52749da8b6a46f3ba2284fdb028a
Kevin Chisholm <kevinchisholm@Kevins-MacBook-Pro.local>
2017-03-14 17:13:06 -0400

    update the join method

--- Array/join.js
+++ Array/join.js
@@ -2,11 +2,23 @@ var foo = ['a', 'b', 'c', 'd', 'e', 'f'];
 
 //the join method combines all elemetns of an array
 //and returns them as a string
+
 //the default separator is a comma: ','
 foo.join(); // 'a,b,c,d,e,f'
 
-//of you can provide a specific separator
-foo.join(','); // 'a,b,c,d,e,f'
-foo.join('-'); // 'a-b-c-d-e-f'
-foo.join(' '); // 'a b c d e f'
-foo.join('/'); // 'a/b/c/d/e/f'
+console.warn('join using the default separator');
+console.log(foo.join()); // 'a b c d e f'
+
+//of you can provide a specific separator:
+
+console.warn('join using a space');
+console.log(foo.join(' ')); // 'a b c d e f'
+
+console.warn('join using: ","');
+console.log(foo.join(',')); // 'a,b,c,d,e,f'
+
+console.warn('join using: "-"');
+console.log(foo.join('-')); // 'a-b-c-d-e-f'
+
+console.warn('join using: "/"');
+console.log(foo.join('/')); // 'a/b/c/d/e/f'
