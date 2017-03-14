b65327f6e095560ae4320e87cb1068bcbedb0ab1
Kevin Chisholm <kevinchisholm@Kevins-Mac-mini.local>
2017-03-14 14:37:56 -0400

    add string split

---
+++ String/split.js
@@ -0,0 +1,15 @@
+var testString = 'I want to be an array';
+
+//the split method turns a string into an array.
+
+console.info('if we pass no arguments, then every letter becomes an array element');
+console.dir(testString.split(''));
+
+console.info('if we pass argument, then that determines where the string is split');
+console.dir(testString.split(' '));
+
+console.info('if we split on " be ", then whatever is on either side of it becomes an array element');
+console.dir(testString.split(' be '));
+
+console.info('...and you can do some pretty funky stuff.');
+console.dir(testString.split('a'));
