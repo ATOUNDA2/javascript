1005c5eee3ff2755fe56b8fda0e8584a8fbb4fbc
Kevin Chisholm <kevin212@pobox.com>
2017-03-15 10:00:52 -0400

    fix join comment

--- Array/join.js
+++ Array/join.js
@@ -4,10 +4,8 @@ var foo = ['a', 'b', 'c', 'd', 'e', 'f'];
 //and returns them as a string
 
 //the default separator is a comma: ','
-foo.join(); // 'a,b,c,d,e,f'
-
 console.warn('join using the default separator');
-console.log(foo.join()); // 'a b c d e f'
+console.log(foo.join()); // 'a,b,c,d,e,f'
 
 //or you can provide a specific separator:
 
